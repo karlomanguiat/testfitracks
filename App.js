8c642e03254d24374c5887420c5a6b518bdcdd54
karlomanguiat <gdmanguiat@up.edu.ph>
2019-02-05 14:37:37 +0800

    Update App.js

--- App.js
+++ App.js
@@ -12,13 +12,6 @@ import {Platform, StyleSheet, Text, View} from 'react-native';
 //import Main from './components/Main'
 import AddForm from './components/AddForm'
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
 type Props = {};
 export default class App extends Component<Props> {
   render() {
