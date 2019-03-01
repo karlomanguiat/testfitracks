23b54ace5e9df9da50338c85264965102e25c44c
karlomanguiat <gdmanguiat@up.edu.ph>
2019-03-01 20:34:48 +0800

    First commit.

---
+++ app/src/main/java/com/example/myapplication/FoodIntakeFragment.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class FoodIntakeFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_food_intake_fragment, container, false);
+    }
+}
